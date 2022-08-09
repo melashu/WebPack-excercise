e7271f8c2563ceabb74a861cc528ff1afdeb8caf
melashu <meshu102@outlook.com>
2022-08-09 19:33:30 +0300

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import '../css/main.css'
+import '../css/style.css'
+
+console.log('Work')
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Work Well",
+      template: "./src/index.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
